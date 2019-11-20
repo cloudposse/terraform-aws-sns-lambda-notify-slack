b3ec358d311fe6d3f9e417a909704e40caa7acd8
aknysh <andriy.knysh@gmail.com>
2019-11-20 16:31:55 -0500

    Convert to TF 0.12. Add Codefresh test pipeline

--- docs/terraform.md
+++ docs/terraform.md
@@ -1,21 +1,19 @@
-
 ## Inputs
 
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
-| attributes | List of attributes to add to label. | list | `<list>` | no |
-| create_sns_topic | Whether to create new SNS topic | string | `true` | no |
-| create_with_kms_key | Whether to create resources with KMS encryption | string | `false` | no |
+| attributes | List of attributes to add to label. | list(string) | `<list>` | no |
+| create_sns_topic | Whether to create new SNS topic | bool | `true` | no |
 | delimiter | The delimiter to be used in labels. | string | `-` | no |
-| enabled | Whether to create all resources | string | `true` | no |
+| enabled | Whether to create all resources | bool | `true` | no |
 | kms_key_arn | ARN of the KMS key used for decrypting slack webhook url | string | `` | no |
 | name | Name (unique identifier for app or service) | string | - | yes |
-| namespace | Namespace (e.g. `cp` or `cloudposse`) | string | - | yes |
+| namespace | Namespace (e.g. `cp` or `cloudposse`) | string | `` | no |
 | slack_channel | The name of the channel in Slack for notifications | string | - | yes |
 | slack_emoji | A custom emoji that will appear on Slack messages | string | `:aws:` | no |
 | slack_username | The username that will appear on Slack messages | string | - | yes |
 | slack_webhook_url | The URL of Slack webhook | string | - | yes |
 | sns_topic_name | Name of the SNS topic to subscribe to. | string | `` | no |
-| stage | Stage (e.g. `prod`, `dev`, `staging`) | string | - | yes |
-| tags | Map of key-value pairs to use for tags. | map | `<map>` | no |
+| stage | Stage (e.g. `prod`, `dev`, `staging`) | string | `` | no |
+| tags | Map of key-value pairs to use for tags. | map(string) | `<map>` | no |
 
--- variables.tf
+++ variables.tf
@@ -1,83 +1,79 @@
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name (unique identifier for app or service)"
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `cp` or `cloudposse`)"
+  default     = ""
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   description = "The delimiter to be used in labels."
   default     = "-"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   description = "List of attributes to add to label."
   default     = []
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   description = "Map of key-value pairs to use for tags."
   default     = {}
 }
 
 variable "enabled" {
-  type        = "string"
+  type        = bool
   description = "Whether to create all resources"
-  default     = "true"
+  default     = true
 }
 
 variable "create_sns_topic" {
-  type        = "string"
+  type        = bool
   description = "Whether to create new SNS topic"
-  default     = "true"
-}
-
-variable "create_with_kms_key" {
-  type        = "string"
-  description = "Whether to create resources with KMS encryption"
-  default     = "false"
+  default     = true
 }
 
 variable "slack_webhook_url" {
-  type        = "string"
+  type        = string
   description = "The URL of Slack webhook"
 }
 
 variable "slack_channel" {
-  type        = "string"
+  type        = string
   description = "The name of the channel in Slack for notifications"
 }
 
 variable "slack_username" {
-  type        = "string"
+  type        = string
   description = "The username that will appear on Slack messages"
 }
 
 variable "slack_emoji" {
-  type        = "string"
+  type        = string
   description = "A custom emoji that will appear on Slack messages"
   default     = ":aws:"
 }
 
 variable "kms_key_arn" {
-  type        = "string"
+  type        = string
   description = "ARN of the KMS key used for decrypting slack webhook url"
   default     = ""
 }
 
 variable "sns_topic_name" {
-  type        = "string"
+  type        = string
   description = "Name of the SNS topic to subscribe to."
   default     = ""
 }
