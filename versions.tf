b40130a4c5701fb217b446f575a44d29480fd826
Leonardo Zavala <lezavala@globe.life>
2021-01-18 08:12:00 -0800

    Updates version.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,26 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.12.26"
 
   required_providers {
-    aws      = "~> 2.0"
-    template = "~> 2.0"
-    local    = "~> 1.3"
-    archive  = "~> 1.3"
-    null     = "~> 2.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.3"
+    }
+    archive  = {
+      source = "hashicorp/archive"
+      version = ">=1.3"
+    }
   }
 }
