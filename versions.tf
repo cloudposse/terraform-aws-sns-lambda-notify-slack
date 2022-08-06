fa8fb6a112cda58bec073c9d5ce1621bb89985f9
yutachaos <18604471+yutachaos@users.noreply.github.com>
2022-08-06 16:11:50 +0900

    Removed unused providers

--- versions.tf
+++ versions.tf
@@ -6,21 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 2.0"
     }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = ">=1.3"
-    }
   }
 }
