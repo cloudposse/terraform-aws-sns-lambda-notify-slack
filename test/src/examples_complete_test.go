1e9fa52e073230c33899dd24bc907877a1110952
Igor Rodionov <goruha@gmail.com>
2024-07-11 19:51:06 +0300

    Update examples_complete_test.go

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -8,13 +8,19 @@ import (
 // Test the Terraform module in examples/complete using Terratest.
 func TestExamplesComplete(t *testing.T) {
 	t.Parallel()
-
+	
+	randID := strings.ToLower(random.UniqueId())
+	attributes := []string{randID}
+	
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: "../../examples/complete",
 		Upgrade:      true,
 		// Variables to pass to our Terraform code using -var-file options
 		VarFiles: []string{"fixtures.us-east-2.tfvars"},
+		Vars: map[string]interface{}{
+			"attributes": attributes,
+		},		
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
